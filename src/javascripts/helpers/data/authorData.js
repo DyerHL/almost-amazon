188db61fab29da6a34a37a9380f1bc9813fb8dd2
Halie Dyer <Halie.L.Dyer@gmail.com>
2021-08-29 18:23:02 -0400

    completed CRUD

--- src/javascripts/helpers/data/authorData.js
+++ src/javascripts/helpers/data/authorData.js
@@ -12,19 +12,54 @@ const getAuthors = () => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
+// GET SINGLE AUTHOR
+const getSingleAuthor = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/authors/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
+
 // DELETE AUTHOR
+const deleteAuthor = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/authors/${firebaseKey}.json`)
+    .then(() => {
+      getAuthors().then(resolve);
+    })
+    .catch(reject);
+});
+
 // CREATE AUTHOR
 const createAuthor = (authorObj) => new Promise((resolve, reject) => {
   axios.post(`${dbUrl}/authors.json`, authorObj)
     .then((response) => {
       const body = { firebaseKey: response.data.name };
-      axios.patch(`${dbUrl}/authors.json/${response.data.name}`, body)
+      axios.patch(`${dbUrl}/authors/${response.data.name}.json`, body)
         .then(() => {
-          getAuthors().then(((authorsArray) => resolve(authorsArray)));
+          getAuthors().then((authorsArray) => resolve(authorsArray));
         });
     }).catch((error) => reject(error));
 });
+
 // UPDATE AUTHOR
+const updateAuthor = (authorObj) => new Promise((resolve, reject) => {
+  axios.patch(`${dbUrl}/authors/${authorObj.firebaseKey}.json`, authorObj)
+    .then(() => getAuthors().then(resolve))
+    .catch(reject);
+});
 // SEARCH AUTHORS
 
-export { getAuthors, createAuthor };
+// FITLER AUTHORS
+const filterAuthors = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/authors.json?orderBy="favorite"&equalTo=true`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
+
+export {
+  getAuthors,
+  createAuthor,
+  filterAuthors,
+  deleteAuthor,
+  updateAuthor,
+  getSingleAuthor
+};
--- src/javascripts/helpers/data/bookData.js
+++ src/javascripts/helpers/data/bookData.js
@@ -11,13 +11,28 @@ const getBooks = () => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
+// GET SINGLE B00K
+const getSingleBook = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/books/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
+
 // DELETE BOOK
+const deleteBook = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/books/${firebaseKey}.json`)
+    .then(() => {
+      getBooks().then(resolve);
+    })
+    .catch(reject);
+});
+
 // CREATE BOOK
 const createBook = (bookObj) => new Promise((resolve, reject) => {
   axios.post(`${dbUrl}/books.json`, bookObj)
     .then((response) => {
       const body = { firebaseKey: response.data.name };
-      axios.patch(`${dbUrl}/books.json/${response.data.name}`, body)
+      axios.patch(`${dbUrl}/books/${response.data.name}.json`, body)
         .then(() => {
           getBooks().then(((booksArray) => resolve(booksArray)));
         });
@@ -25,6 +40,26 @@ const createBook = (bookObj) => new Promise((resolve, reject) => {
 });
 
 // UPDATE BOOK
+const updateBook = (bookObj) => new Promise((resolve, reject) => {
+  axios.patch(`${dbUrl}/books/${bookObj.firebaseKey}.json`, bookObj)
+    .then(() => getBooks().then(resolve))
+    .catch(reject);
+});
+
 // SEARCH BOOKS
 
-export { getBooks, createBook };
+// FILTER BOOKS ON SALE
+const booksOnSale = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/books.json?orderBy="sale"&equalTo=true`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
+
+export {
+  getBooks,
+  getSingleBook,
+  createBook,
+  booksOnSale,
+  deleteBook,
+  updateBook
+};
